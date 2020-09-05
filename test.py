0ad8412dc53e8cba38a5e51c63e7368b4946eda9
Ildarko99 <ildar.yakhin@gmail.com>
2020-09-05 17:54:48 +0300

    3.1 base.html created

---
+++ test.py
@@ -0,0 +1,27 @@
+data = []
+print(data)
+with open('mainapp/templates/mainapp/index.html', 'r') as f:
+    index_html = f.read()
+    f.close()
+
+with open('mainapp/templates/mainapp/checkout.html', 'r') as f:
+    checkout_html = f.read()
+    f.close()
+
+with open('mainapp/templates/mainapp/contact.html', 'r') as f:
+    contact_html = f.read()
+    f.close()
+
+with open('mainapp/templates/mainapp/products.html', 'r') as f:
+    products_html = f.read()
+    f.close()
+
+with open('mainapp/templates/mainapp/single.html', 'r') as f:
+    single_html = f.read()
+    f.close()
+
+print(data)
+
+div = 
+
+'''

448d8b0290cc0fd239368a20f23094445a55f58a
Ildarko99 <ildar.yakhin@gmail.com>
2020-09-06 10:48:54 +0300

    4.1. includes created, with dynamic titles

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,19 +5,43 @@ from django.shortcuts import render
 # root is "mainapp/templates/" folder
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'page_title': 'Luxury watches | Home page',
+        'breadcrumbs_active': 'Home'
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def checkout(request):
-    return render(request, 'mainapp/checkout.html')
+    context = {
+        'page_title': 'Luxury watches | Basket',
+        'breadcrumbs_active': 'Basket'
+    }
+    return render(request, 'mainapp/checkout.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'page_title': 'Luxury watches | Contacts page',
+        'breadcrumbs_active': 'Contacts'
+    }
+    return render(request, 'mainapp/contact.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'page_title': 'Luxury watches | Our watches',
+        'breadcrumbs_active': 'Products'
+    }
+    return render(request, 'mainapp/products.html', context)
 
 def single(request):
-    return render(request, 'mainapp/single.html')
+    context = {
+        'page_title': 'Luxury watches | Product page',
+        'breadcrumbs_active': 'Product'
+    }
+    return render(request, 'mainapp/single.html', context)
 
 def blog(request):
-    return render(request, 'mainapp/typo.html')
+    context = {
+        'page_title': 'Luxury watches | Our blog',
+        'breadcrumbs_active': 'Blog'
+    }
+    return render(request, 'mainapp/typo.html', context)

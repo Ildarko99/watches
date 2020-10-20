63cad879549272563e17538d3e452da3d2738746
Ildarko99 <ildar.yakhin@gmail.com>
2020-10-20 21:39:26 +0300

    Lesson 7 - return to previous product after login \- dynamic basket with ajax

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -6,6 +6,9 @@ app_name = 'basketapp'
 urlpatterns = [
     path('', basketapp.index, name='basketapp'),
     path('add/product/<int:pk>/', basketapp.add, name='add'),
+
+    path('change/<int:pk>/quantity/<int:quantity>/', basketapp.change),
+
     path('delete/basket/item/<int:pk>/', basketapp.delete, name='delete'),
     path('delete/basket/item/all/', basketapp.delete_all, name='delete_all'),
     # re_path('^add/product/(?P<pk>\d+)/$', basketapp.add, name='add'),
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
+from django.template import loader
 from django.urls import reverse
 
 from basketapp.models import BasketItem
@@ -32,7 +33,7 @@ def add(request, pk):
         )
 
     product = get_object_or_404(Product, pk=pk)
-    basket = BasketItem(user=request.user, product=product)
+    basket = BasketItem.objects.filter(user=request.user, product=product).first()
 
     if not basket:
         basket = BasketItem(user=request.user, product=product)  # not in db
@@ -44,10 +45,11 @@ def add(request, pk):
 
 @login_required
 def delete(request, pk):
-    basket = get_object_or_404(BasketItem, pk=pk).delete()
+    get_object_or_404(BasketItem, pk=pk).delete()
     # basket_items = request.user.basketitem_set.all() если не задано RELATED_NAME в классе BasketItem
     basket_items = request.user.user_basket.all()
     return HttpResponseRedirect(reverse('basketapp:basketapp'))
+
 
 @login_required
 def delete_all(request):
@@ -55,3 +57,32 @@ def delete_all(request):
     if basket_items:
         BasketItem.objects.all().delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def change(request, pk, quantity):
+    if request.is_ajax():
+        basket_item = BasketItem.objects.filter(pk=pk).first()
+        print(basket_item)
+        if quantity == 0:
+            basket_item.delete()
+        else:
+            # quantity validation
+            basket_item.quantity = quantity
+            basket_item.save()
+
+        context = {
+            'basket_items': request.user.user_basket.all(),
+        }
+
+        basket_items = loader.render_to_string(
+            'basketapp/inc__basket_items.html',
+            context=context,
+            request=request,  # csrf token update
+        )
+
+        return JsonResponse({
+            'basket_items': basket_items,
+            # 'basket_cost': user.basket_cost(),
+            # 'basket_total_quantity': user.basket_total_quantity(),
+            # 'basket_item': basket_item,  # serialization -> drf
+        })
